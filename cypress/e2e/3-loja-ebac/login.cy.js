76c89c1afc0bb6341d228b6138f6dd7d457350eb
Enzo Esteves Scartezini <enzoesteves20@gmail.com>
2025-07-04 14:40:26 -0300

    login com dados e fixture

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
     afterEach(() => {
@@ -31,6 +32,22 @@ it('Deve exibir uma mensgem de erro ao inserir senha inválida', () => {
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail enzoesteves20@gmail.com está incorreta. Perdeu a senha?')
         cy.get('.woocommerce-error').should('exist')
+});
+
+it('Deve fazer login com sucesso - Usando massa de dados', () => {
+        cy.get('#username').type(perfil.usario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, enzoesteves20 (não é enzoesteves20? Sair)')
+});
+
+it.only('Deve fazer login com sucesso - Fixture', () => {
+    cy.fixture('perfil').then(dados =>{
+        cy.get('#username').type(perfil.usario)
+        cy.get('#password').type(perfil.senha , {log: false})
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, enzoesteves20 (não é enzoesteves20? Sair)')
+})
 });
 
 })

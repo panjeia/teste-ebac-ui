849b62e35fd1db7ad23f64c7f352d0392cc29891
Enzo Esteves Scartezini <enzoesteves20@gmail.com>
2025-07-04 13:56:33 -0300

    teste da guia de produtos

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block')
+            //.first()
+            //.last()
+            //.eq(2)
+            .contains('Apollo Running Short')
+            .click()
+        
+        cy.get('#tab-title-description > a').should('exist')
+
+    });
+});
